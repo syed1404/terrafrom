97b98c1f93a0ed613b38d7f42636e01485603d45
syed1404 <yacoobzulfi@gmail.com>
2024-09-02 13:05:32 +0530

    Update main.tf

--- Deployments/modules/security group/main.tf
+++ Deployments/modules/security group/main.tf
@@ -103,26 +103,6 @@ output "instance_sg1" {
 resource "aws_security_group" "ailb_sg" {
   vpc_id = var.vpc_id
  
-  # Allow inbound traffic
-  dynamic "ingress" {
-    for_each = var.ingress_rule
-    content {
-    from_port   = ingress.value.from_port
-    to_port     = ingress.value.to_port
-    protocol    = ingress.value.protocol
-    cidr_blocks = ingress.value.cidr_block
-    }
-}
-  # Allow all outbound traffic (default behavior)
-  dynamic "egress" {
-    for_each = var.egress_rule
-    content {
-    from_port   = egress.value.from_port
-    to_port     = egress.value.to_port
-    protocol    = egress.value.protocol
-    cidr_blocks = egress.value.cidr_block
-    }
-  }
   tags = {
     Name = var.sg_name3
   }
